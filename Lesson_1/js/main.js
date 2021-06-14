150bd31e8ee5d2cf521de817f25c51f0c1f32812
Vladimir Peshkov <peshkovvladimir1990@gmail.com>
2021-06-14 13:38:45 +0300

    Домашнее задание

--- Lesson_1/js/main.js
+++ Lesson_1/js/main.js
@@ -1,22 +1,23 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
+    { id: 1, title: 'Notebook', price: 2000 },
+    { id: 2, title: 'Mouse', price: 20 },
+    { id: 3, title: 'Keyboard', price: 200 },
+    { id: 4, title: 'Gamepad', price: 50 },
 ];
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
-const renderProduct = (title, price) => {
+const renderProduct = (title = "product", price = 1000) => {
     return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
+                <img class="product-img" src="http://placehold.it/250x250/">
+                <h3 class="product-title">${title}</h3>
+                <p class="product-price">${price}$</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title,item.price));
+    const productsList = list.map(item => renderProduct(item.title, item.price));
     console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList;
+    document.querySelector('.products').innerHTML = productsList.join('');
 };
 
 renderPage(products);
